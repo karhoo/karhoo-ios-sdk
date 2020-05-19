eab6e43a3e82e97beac29d188a2b43230992b35e
Jeevan Thandi <jeevanthandi@googlemail.com>
2020-05-19 11:35:51 +0100

    MOB-XXXX added comments field (#7)
    * added comments field
    
    * tests
    
    * version bump

--- KarhooSDK/Api/Request/TripBooking.swift
+++ KarhooSDK/Api/Request/TripBooking.swift
@@ -14,20 +14,24 @@ public struct TripBooking: KarhooCodableModel, Equatable {
     public let passengers: Passengers
     public let flightNumber: String?
     public var paymentNonce: String?
+    public var comments: String?
 
     public init(quoteId: String = "",
                 passengers: Passengers = Passengers(),
                 flightNumber: String? = nil,
-                paymentNonce: String? = nil) {
+                paymentNonce: String? = nil,
+                comments: String? = nil) {
         self.quoteId = quoteId
         self.passengers = passengers
         self.flightNumber = flightNumber
         self.paymentNonce = paymentNonce
+        self.comments = comments
     }
 
     enum CodingKeys: String, CodingKey {
         case quoteId = "quote_id"
         case passengers
+        case comments
         case flightNumber = "flight_number"
         case paymentNonce = "payment_nonce"
     }
