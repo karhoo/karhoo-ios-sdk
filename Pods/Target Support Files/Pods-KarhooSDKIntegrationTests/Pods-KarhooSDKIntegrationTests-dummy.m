b9995daed99b8271584ccf3c39a6178c2169d39b
Jeevan Thandi <jeevan@airlatech.com>
2020-06-17 13:32:23 +0100

    remove pods

--- Pods/Target Support Files/Pods-KarhooSDKIntegrationTests/Pods-KarhooSDKIntegrationTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KarhooSDKIntegrationTests : NSObject
-@end
-@implementation PodsDummy_Pods_KarhooSDKIntegrationTests
-@end
--- Pods/Target Support Files/Pods-KarhooSDKTests/Pods-KarhooSDKTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KarhooSDKTests : NSObject
-@end
-@implementation PodsDummy_Pods_KarhooSDKTests
-@end
