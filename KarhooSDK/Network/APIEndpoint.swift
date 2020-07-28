65101d09c7497e1ce3980575272350dd141efb6f
Jo Santa Maria <jo.santamaria@karhoo.com>
2020-07-28 16:57:02 +0100

    Update API endpoints for quote v2 and start changes for integration test

--- KarhooSDK/Network/APIEndpoint.swift
+++ KarhooSDK/Network/APIEndpoint.swift
@@ -149,8 +149,8 @@ enum APIEndpoint {
         case .addPaymentDetails: return "v2"
         case .getNonce: return "v2"
         case .paymentSDKToken: return "v2"
-        case .quotes(_ ): return "v1"
-        case .quoteListId: return "v1"
+        case .quotes(_ ): return "v2"
+        case .quoteListId: return "v2"
         default: return "v1"
         }
     }
