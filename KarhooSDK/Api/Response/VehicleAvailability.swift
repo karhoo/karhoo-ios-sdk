1d98354728ae7d9eba111e7a0094d9477bcc4d43
Nurseda Balcioglu <nurseda.balcioglu@karhoo.com>
2020-11-13 17:15:41 +0000

    work completed

--- KarhooSDK/Api/Response/VehicleAvailability.swift
+++ KarhooSDK/Api/Response/VehicleAvailability.swift
@@ -11,17 +11,22 @@ import Foundation
 public struct VehicleAvailability: KarhooCodableModel {
 
     public let classes: [String]
+    public let types: [String]
 
-    public init(classes: [String] = []) {
+    public init(classes: [String] = [],
+                types: [String] = []) {
         self.classes = classes
+        self.types = types
     }
 
     enum CodingKeys: String, CodingKey {
         case classes
+        case types
     }
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.classes = (try? container.decode(Array.self, forKey: .classes)) ?? []
+        self.types = (try? container.decode(Array.self, forKey: .types)) ?? []
     }
 }
