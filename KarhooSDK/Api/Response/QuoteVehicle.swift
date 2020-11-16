93d0c6e0ccb840a23da37cd172f523875237b42b
nurito-burrito <44758040+nurito-burrito@users.noreply.github.com>
2020-11-16 14:42:46 +0000

    work completed (#58)
    Co-authored-by: Nurseda Balcioglu <nurseda.balcioglu@karhoo.com>

--- KarhooSDK/Api/Response/QuoteVehicle.swift
+++ KarhooSDK/Api/Response/QuoteVehicle.swift
@@ -11,23 +11,33 @@ import Foundation
 public struct QuoteVehicle: Codable {
 
     public let vehicleClass: String
+    public let type: String
+    public let tags: [String]
     public let qta: QuoteQta
 
 
     public init(vehicleClass: String = "",
+                type: String = "",
+                tags: [String] = [],
                 qta: QuoteQta = QuoteQta()) {
         self.vehicleClass = vehicleClass
+        self.type = type
+        self.tags = tags
         self.qta = qta
     }
 
     enum CodingKeys: String, CodingKey {
         case vehicleClass = "class"
+        case type
+        case tags
         case qta
     }
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.vehicleClass = (try? container.decode(String.self, forKey: .vehicleClass)) ?? ""
+        self.type = (try? container.decode(String.self, forKey: .type)) ?? ""
+        self.tags = (try? container.decode(Array.self, forKey: .tags)) ?? []
         self.qta = (try? container.decode(QuoteQta.self, forKey: .qta)) ?? QuoteQta()
     }
 }
