ae9b801999ad0a64dc184ccb364a7e35f0d21207
Mostafa Hadian <mostafa.hadian@karhoo.com>
2021-03-02 16:35:36 +0000

    Remove URL suffix (#88)

--- KarhooSDK/Api/Request/Adyen/AdyenPaymentsRequest.swift
+++ KarhooSDK/Api/Request/Adyen/AdyenPaymentsRequest.swift
@@ -11,22 +11,17 @@ import Foundation
 public struct AdyenPaymentsRequest: KarhooRequestModel {
     
     public let paymentsPayload: AdyenDropInPayload
-    public let returnUrlSuffix: String
 
-    public init(paymentsPayload: AdyenDropInPayload = AdyenDropInPayload(),
-                returnUrlSuffix: String = "") {
+    public init(paymentsPayload: AdyenDropInPayload = AdyenDropInPayload()) {
         self.paymentsPayload = paymentsPayload
-        self.returnUrlSuffix = returnUrlSuffix
     }
     
     enum CodingKeys: String, CodingKey {
         case paymentsPayload = "payments_payload"
-        case returnUrlSuffix = "return_url_suffix"
     }
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(returnUrlSuffix, forKey: .returnUrlSuffix)
         try container.encode(paymentsPayload, forKey: .paymentsPayload)
     }
 }
