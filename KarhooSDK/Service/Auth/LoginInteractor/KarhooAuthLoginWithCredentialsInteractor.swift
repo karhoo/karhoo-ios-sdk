72b1967250908378e2bacb460f1ba30ec6072278
EdwardW2 <edward@wilkins-family.org.uk>
2021-07-16 09:10:20 +0100

    Almost finished

--- KarhooSDK/Service/Auth/LoginInteractor/KarhooAuthLoginWithCredentialsInteractor.swift
+++ KarhooSDK/Service/Auth/LoginInteractor/KarhooAuthLoginWithCredentialsInteractor.swift
@@ -12,11 +12,23 @@ final class KarhooAuthLoginWithCredentialsInteractor: AuthLoginWithCredentialsIn
     private var credentials: Credentials?
     private let credentialsRequestSender: RequestSender
     private let userInfoSender: RequestSender
+    private let userDataStore: UserDataStore
+    private let analytics: AnalyticsService
+    private let paymentProviderRequest: RequestSender
+    private let nonceRequestSender: RequestSender
     
     init(credentialsRequestSender: RequestSender = KarhooRequestSender(httpClient: JsonHttpClient.shared),
-         userInfoSender: RequestSender = KarhooRequestSender(httpClient: TokenRefreshingHttpClient.shared)) {
+         userInfoSender: RequestSender = KarhooRequestSender(httpClient: TokenRefreshingHttpClient.shared),
+         userDataStore: UserDataStore = DefaultUserDataStore(),
+         analytics: AnalyticsService = KarhooAnalyticsService(),
+         paymentProviderRequest: RequestSender = KarhooRequestSender(httpClient: JsonHttpClient.shared),
+         nonceRequestSender: RequestSender = KarhooRequestSender(httpClient: TokenRefreshingHttpClient.shared)) {
         self.credentialsRequestSender = credentialsRequestSender
         self.userInfoSender = userInfoSender
+        self.userDataStore = userDataStore
+        self.analytics = analytics
+        self.paymentProviderRequest = paymentProviderRequest
+        self.nonceRequestSender = nonceRequestSender
     }
 
     func set(credentials: Credentials?) {
@@ -25,11 +37,44 @@ final class KarhooAuthLoginWithCredentialsInteractor: AuthLoginWithCredentialsIn
     
     func execute<T>(callback: @escaping CallbackClosure<T>) where T : KarhooCodableModel {
         if credentials == nil { return }
+        guard let userInfoCallback = callback as? CallbackClosure<UserInfo> else {
+            return
+        }
+        //TODO: Figuring how how to sort this
     }
     
     func cancel() {
         credentialsRequestSender.cancelNetworkRequest()
         userInfoSender.cancelNetworkRequest()
+    }
+    
+    private func didLogin(user: UserInfo,
+                          credentials: Credentials) {
+        userDataStore.setCurrentUser(user: user, credentials: credentials)
+        updatePaymentProvider(user: user)
+        analytics.send(eventName: .ssoUserLogIn)
+    }
+
+    private func updatePaymentProvider(user: UserInfo) {
+        paymentProviderRequest.requestAndDecode(payload: nil,
+                                                endpoint: .paymentProvider,
+                                                callback: { [weak self] (result: Result<PaymentProvider>) in
+                                                    let paymentProvider = result.successValue()
+                                                    self?.userDataStore.updatePaymentProvider(paymentProvider: paymentProvider)
+                                                    if paymentProvider?.provider.type == .braintree {
+                                                        self?.updateUserNonce(user: user)
+                                                    }
+                                                })
+    }
+    
+    private func updateUserNonce(user: UserInfo) {
+        let payload = NonceRequestPayload(payer: Payer(user: user),
+                                          organisationId: user.organisations.first?.id ?? "")
+
+        nonceRequestSender.requestAndDecode(payload: payload,
+                                            endpoint: .getNonce) { [weak self] (result: Result<Nonce>) in
+                                                self?.userDataStore.updateCurrentUserNonce(nonce: result.successValue())
+        }
     }
     
 }
