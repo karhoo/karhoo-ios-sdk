8893cf3882e39267fd4788b3750be983623e25c7
EdwardW2 <44901854+EdwardW2@users.noreply.github.com>
2021-11-15 11:11:38 +0000

    Add LoyaltyPoints model

---
+++ KarhooSDK/Api/Response/LoyaltyPoints.swift
@@ -0,0 +1,32 @@
+//
+//  LoyaltyPoints.swift
+//  KarhooSDK
+//
+//  Created by Edward Wilkins on 15/11/2021.
+//  Copyright © 2021 Flit Technologies Ltd. All rights reserved.
+//
+
+import Foundation
+
+public struct LoyaltyPoints: KarhooCodableModel {
+    
+    public let points: Int
+    
+    public init(points: Int = 0) {
+        self.points = points
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case points
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.points = (try? container.decode(Int.self, forKey: .points)) ?? 0
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(points, forKey: .points)
+    }
+}
