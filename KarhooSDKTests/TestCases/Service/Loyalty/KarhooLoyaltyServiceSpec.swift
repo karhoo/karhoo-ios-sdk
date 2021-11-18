e800556683174e4b32ab907dbdcd370ea0062ba1
EdwardW2 <44901854+EdwardW2@users.noreply.github.com>
2021-11-18 12:23:05 +0000

    Add tests

--- KarhooSDKTests/TestCases/Service/Loyalty/KarhooLoyaltyServiceSpec.swift
+++ KarhooSDKTests/TestCases/Service/Loyalty/KarhooLoyaltyServiceSpec.swift
@@ -14,19 +14,37 @@ final class KarhooLoyaltyServiceSpec: XCTestCase {
     private var testObject: KarhooLoyaltyService!
     private var mockLoyaltyBalanceInteractor: MockLoyaltyBalanceInteractor!
     private var mockLoyaltyConversionInteractor: MockLoyaltyConversionInteractor!
+    private var mockLoyaltyStatusInteractor: MockLoyaltyStatusInteractor!
+    private var mockLoyaltyBurnInteractor: MockLoyaltyBurnInteractor!
+    private var mockLoyaltyEarnInteractor: MockLoyaltyEarnInteractor!
+    private var mockLoyaltyPreAuthInteractor: MockLoyaltyPreAuthInteractor!
     
     private let identifier = "some_id"
+    private let currency = "GBP"
+    private let amount = 10
+    private let points = 10
     static let ratesMock = LoyaltyRates(currency: "GBP", points: "10")
     private let loyaltyConversionMock = LoyaltyConversion(version: "123", rates: [ratesMock])
     private let loyaltyBalanceMock = LoyaltyBalance(points: 10, burnable: false)
+    private let loyaltyStatusMock = LoyaltyStatus(balance: 1000, canBurn: false, canEarn: true)
+    private let loyaltyPointsMock = LoyaltyPoints(points: 10)
+    private let loyaltyPreAuthPayloadMock = LoyaltyPreAuthPayload(currency: "GBP", points: 100, flexpay: false, membership: "123A")
     
     override func setUp() {
         super.setUp()
         
         mockLoyaltyBalanceInteractor = MockLoyaltyBalanceInteractor()
         mockLoyaltyConversionInteractor = MockLoyaltyConversionInteractor()
+        mockLoyaltyStatusInteractor = MockLoyaltyStatusInteractor()
+        mockLoyaltyBurnInteractor = MockLoyaltyBurnInteractor()
+        mockLoyaltyEarnInteractor = MockLoyaltyEarnInteractor()
+        mockLoyaltyPreAuthInteractor = MockLoyaltyPreAuthInteractor()
         testObject = KarhooLoyaltyService(loyaltyBalanceInteractor: mockLoyaltyBalanceInteractor,
-                                          loyaltyConversionInteractor: mockLoyaltyConversionInteractor)
+                                          loyaltyConversionInteractor: mockLoyaltyConversionInteractor,
+                                          loyaltyStatusInteractor: mockLoyaltyStatusInteractor,
+                                          loyaltyBurnInteractor: mockLoyaltyBurnInteractor,
+                                          loyaltyEarnInteractor: mockLoyaltyEarnInteractor,
+                                          loyaltyPreAuthInteractor: mockLoyaltyPreAuthInteractor)
     }
     
     /**
@@ -90,6 +108,103 @@ final class KarhooLoyaltyServiceSpec: XCTestCase {
 
         let expectedError = TestUtil.getRandomError()
         mockLoyaltyConversionInteractor.triggerFail(error: expectedError)
+
+        XCTAssert(expectedError.equals(result?.errorValue()))
+    }
+    
+    func testLoyaltyStatusSuccess() {
+        let call = testObject.getLoyaltyStatus(identifier: identifier)
+        
+        var result: Result<LoyaltyStatus>?
+        call.execute(callback: { result = $0 })
+        
+        mockLoyaltyStatusInteractor.triggerSuccess(result: loyaltyStatusMock)
+        
+        XCTAssertEqual(1000, result?.successValue()?.balance)
+        XCTAssertEqual(false, result?.successValue()?.canBurn)
+        XCTAssertEqual(true, result?.successValue()?.canEarn)
+    }
+    
+    func testLoyaltyStatusFail() {
+        let call = testObject.getLoyaltyStatus(identifier: identifier)
+        
+        var result: Result<LoyaltyStatus>?
+        call.execute(callback: { result = $0 })
+        
+        let expectedError = TestUtil.getRandomError()
+        mockLoyaltyStatusInteractor.triggerFail(error: expectedError)
+        
+        XCTAssert(expectedError.equals(result?.errorValue()))
+    }
+    
+    func testLoyaltyBurnSuccess() {
+        let call = testObject.getLoyaltyBurn(identifier: identifier, currency: currency, amount: amount)
+        
+        var result: Result<LoyaltyPoints>?
+        call.execute(callback: { result = $0 })
+        
+        mockLoyaltyBurnInteractor.triggerSuccess(result: loyaltyPointsMock)
+        
+        XCTAssertEqual(10, result?.successValue()?.points)
+    }
+    
+    func testLoyaltyBurnFail() {
+        let call = testObject.getLoyaltyBurn(identifier: identifier, currency: currency, amount: amount)
+
+        var result: Result<LoyaltyPoints>?
+        call.execute(callback: { result = $0 })
+
+        let expectedError = TestUtil.getRandomError()
+        mockLoyaltyBurnInteractor.triggerFail(error: expectedError)
+
+        XCTAssert(expectedError.equals(result?.errorValue()))
+    }
+    
+    func testLoyaltyEarnSuccess() {
+        let call = testObject.getLoyaltyEarn(identifier: identifier, currency: currency, amount: amount, points: points)
+        
+        var result: Result<LoyaltyPoints>?
+        call.execute(callback: { result = $0 })
+        
+        mockLoyaltyEarnInteractor.triggerSuccess(result: loyaltyPointsMock)
+        
+        XCTAssertEqual(10, result?.successValue()?.points)
+    }
+    
+    func testLoyaltyEarnFail() {
+        let call = testObject.getLoyaltyEarn(identifier: identifier, currency: currency, amount: amount, points: points)
+
+        var result: Result<LoyaltyPoints>?
+        call.execute(callback: { result = $0 })
+
+        let expectedError = TestUtil.getRandomError()
+        mockLoyaltyEarnInteractor.triggerFail(error: expectedError)
+
+        XCTAssert(expectedError.equals(result?.errorValue()))
+    }
+    
+    func testLoyaltyPreAuthSuccess() {
+        let call = testObject.getLoyaltyPreAuth(identifier: identifier)
+        
+        var result: Result<LoyaltyPreAuthPayload>?
+        call.execute(callback: { result = $0 })
+        
+        mockLoyaltyPreAuthInteractor.triggerSuccess(result: loyaltyPreAuthPayloadMock)
+        
+        XCTAssertEqual("GBP", result?.successValue()?.currency)
+        XCTAssertEqual(100, result?.successValue()?.points)
+        XCTAssertEqual(false, result?.successValue()?.flexpay)
+        XCTAssertEqual("123A", result?.successValue()?.membership)
+    }
+    
+    func testLoyaltyPreAuthFail() {
+        let call = testObject.getLoyaltyPreAuth(identifier: identifier)
+
+        var result: Result<LoyaltyPreAuthPayload>?
+        call.execute(callback: { result = $0 })
+
+        let expectedError = TestUtil.getRandomError()
+        mockLoyaltyPreAuthInteractor.triggerFail(error: expectedError)
 
         XCTAssert(expectedError.equals(result?.errorValue()))
     }
