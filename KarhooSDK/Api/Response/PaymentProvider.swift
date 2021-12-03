882dec3730b78e9f91a02432141aaec3fa0cb32e
Mostafa Hadian <mostafa.hadian@karhoo.com>
2021-12-03 16:48:59 +0000

    Fix for paymemt provider model

--- KarhooSDK/Api/Response/PaymentProvider.swift
+++ KarhooSDK/Api/Response/PaymentProvider.swift
@@ -9,14 +9,11 @@ import Foundation
 
 public struct PaymentProvider : KarhooCodableModel {
     public let provider: Provider
-    public let loyaltyProgammes: [LoyaltyProgramme]
     public let loyaltyProgamme: LoyaltyProgramme
 
     public init(provider: Provider = Provider(),
-                loyaltyProgammes: [LoyaltyProgramme] = [],
                 loyaltyProgamme: LoyaltyProgramme = LoyaltyProgramme()) {
         self.provider = provider
-        self.loyaltyProgammes = loyaltyProgammes
         self.loyaltyProgamme = loyaltyProgamme
     }
     
@@ -24,13 +21,11 @@ public struct PaymentProvider : KarhooCodableModel {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
         self.provider = (try? container.decode(Provider.self, forKey: .provider)) ?? Provider()
-        self.loyaltyProgammes = (try? container.decode([LoyaltyProgramme].self, forKey: .loyaltyProgammes)) ?? []
         self.loyaltyProgamme = (try? container.decode(LoyaltyProgramme.self, forKey: .loyaltyProgamme)) ?? LoyaltyProgramme()
     }
     
     enum CodingKeys: String, CodingKey {
         case provider
-        case loyaltyProgammes = "loyalty_programmes"
         case loyaltyProgamme = "loyalty_programme"
     }
 }
