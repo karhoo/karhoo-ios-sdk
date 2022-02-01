6e78dcb6bc7a6b0bb2d88a3e3ddbf50418ead850
Diana Petrea <diana.petrea@karhoo.com>
2022-02-01 18:35:59 +0200

    add minor tweak

--- KarhooSDK/Api/Request/Adyen/AdyenPaymentMethodsRequest.swift
+++ KarhooSDK/Api/Request/Adyen/AdyenPaymentMethodsRequest.swift
@@ -9,10 +9,10 @@ import Foundation
 
 public struct AdyenPaymentMethodsRequest: Codable, KarhooCodableModel {
     
-    // The platform where a payment transaction takes place. This field can be used for filtering out payment methods that are only available on specific platforms. Possible values: * iOS * Android * Web
+    /// The platform where a payment transaction takes place. This field can be used for filtering out payment methods that are only available on specific platforms. Possible values: * iOS * Android * Web
     public let channel: String
     public var amount: AdyenAmount?
-    // The combination of a language code and a country code to specify the language to be used in the payment.
+    /// The combination of a language code and a country code to specify the language to be used in the payment.
     public var shopperLocale: String?
 
     public init(channel: String = "iOS",
