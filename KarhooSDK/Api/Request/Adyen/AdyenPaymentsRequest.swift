b3b5642d4b91a15bf041d62848ee350e10854e46
Aleksander Wedrychowski <aleksander.wedrychowski@karhoo.com>
2022-12-21 11:51:20 +0100

    new filed added to adyen payments request

--- KarhooSDK/Api/Request/Adyen/AdyenPaymentsRequest.swift
+++ KarhooSDK/Api/Request/Adyen/AdyenPaymentsRequest.swift
@@ -11,21 +11,29 @@ import Foundation
 public struct AdyenPaymentsRequest: KarhooRequestModel {
     
     public let paymentsPayload: AdyenDropInPayload
+    public let consentModeSupported: Bool
     public let supplyPartnerID: String
 
-    public init(paymentsPayload: AdyenDropInPayload = AdyenDropInPayload(), supplyPartnerID: String) {
+    public init(
+        paymentsPayload: AdyenDropInPayload = AdyenDropInPayload(),
+        consentModeSupported: Bool = true,
+        supplyPartnerID: String
+    ) {
         self.paymentsPayload = paymentsPayload
+        self.consentModeSupported = consentModeSupported
         self.supplyPartnerID = supplyPartnerID
     }
     
     enum CodingKeys: String, CodingKey {
         case paymentsPayload = "payments_payload"
+        case consentModeSupported = "consent_mode_supported"
         case supplyPartnerID = "supply_partner_id"
     }
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(paymentsPayload, forKey: .paymentsPayload)
+        try container.encode(consentModeSupported, forKey: .consentModeSupported)
         try container.encode(supplyPartnerID, forKey: .supplyPartnerID)
     }
 }
